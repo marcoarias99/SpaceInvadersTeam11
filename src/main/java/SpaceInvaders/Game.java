8a5ceac9d53a8201acebd7b8ef4e7b35b851c651
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2019-08-07 02:18:40 -0600

    Add files via upload

---
+++ src/main/java/SpaceInvaders/Game.java
@@ -0,0 +1,70 @@
+package SpaceInvaders.internal;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Game {
+    public static Game main;
+    private Window window;
+    private Input input;
+    private Screen screen;
+    private Thread gameThread;
+
+    public Game(Window window) {
+        this.window = window;
+        input = new Input();
+        input.registerWindowInputs(window);
+        main = this;
+    }
+
+    public Input getInput() {
+        return input;
+    }
+
+    public void setScreen(Screen newScreen) {
+        if (screen != null) screen.dispose();
+        screen = newScreen;
+        screen.create();
+    }
+
+    public float getWidth() {
+        return window.getWidth();
+    }
+
+    public float getHeight() {
+        return window.getHeight();
+    }
+
+    void startGameLoop() {
+        new AnimationTimer() {
+            private long prev = System.nanoTime();
+
+            @Override
+            public void handle(long now) {
+                float diff = (now - prev) / 1000000000.0f;
+                step(diff);
+                prev = now;
+            }
+        }.start();
+    }
+
+    void step(float deltaTime) {
+        if (screen == null) {
+            throw new RuntimeException("Screen is null! Did you forget to call #setScreen(...)?");
+        }
+        input.prepareNextFrame();
+        screen.update(deltaTime);
+
+        GraphicsContext graphics = window.getGraphics();
+
+        // Clear the background
+        Paint paint = graphics.getFill();
+        graphics.setFill(Color.BLACK);
+        graphics.fillRect(0, 0, window.getWidth(), window.getHeight());
+        graphics.setFill(paint);
+
+        screen.render(graphics);
+    }
+}
