3e28180052174705ca3634a286770d87f8b22955
UYScuti1708 <williamnguyen210@yahoo.com>
2019-08-07 03:01:57 -0600

    Create class for multiple game screens

---
+++ src/internal/Screen.java
@@ -0,0 +1,10 @@
+package internal;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public interface Screen {
+    void create();
+    void update(float deltaTime);
+    void render(GraphicsContext g);
+    void dispose();
+}
