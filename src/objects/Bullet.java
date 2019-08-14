cb3786c6b5f5f0891c7f7316b2c8f2f6b15f99e0
mohamedmohamed1 <52938358+mohamedmohamed1@users.noreply.github.com>
2019-08-13 22:09:33 -0600

    Update Bullet.java

--- src/objects/Bullet.java
+++ src/objects/Bullet.java
@@ -1,13 +1,20 @@
 package objects;
 
 public class Bullet extends GameObject {
+	
+	// Instance variables for the speed, width, height of the bullet
+	
     public static final float SPEED = 430.0f;
     public static final float WIDTH = 10.0f;
     public static final float HEIGHT = 15.0f;
+    
+    // Constructor 
 
     public Bullet(int x, int y) {
         initShot(x, y);
     }
+    
+    // Updates the position
 
     @Override
     public void update(float deltaTime) {
@@ -17,9 +24,11 @@ public class Bullet extends GameObject {
     private void initShot(int x, int y) {
         getPosition().set(x, y);
     }
+    
+    // Getter for the image of the bomb, to display the bomb 
 
 	@Override
 	public String getImageName() {
-		return "resources/bullet.png";
+		return "resources/bomb.png";
 	}
 }
