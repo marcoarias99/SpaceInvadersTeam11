3fffa74352cf6f22e7bd0d47578539493e754b89
marcoarias99 <44210044+marcoarias99@users.noreply.github.com>
2019-08-13 22:21:32 -0600

    Comments added to Alien2.java

--- src/objects/Alien2.java
+++ src/objects/Alien2.java
@@ -1,33 +1,50 @@
 package objects;
 
 public class Alien2 extends Alien {
-
+	/*
+	Red Alien class
+	Second enemy for player which will apear after level 3
+	x and y coordinates for Aliens movements
+	*/
 	public Alien2(float x, float y) {
 		super(x, y);
-		// TODO Auto-generated constructor stub
 	}
 
+	/*
+	Overide method used with deltaTime to be able allow Red alien to 
+	update its position inside the game.
+
+	Also allowing Alien shoot and the bombs velocity
+	*/
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
-		
-		
 	}
 	
 	@Override
-	public void spawnBomb() {
-		super.spawnBomb();
-		getBomb().setSpeed(500.0f);
+	public Bomb getNewBomb() {
+		Bomb b = super.getNewBomb();
+		b.setSpeed(200.0f);
+		return b;
 	}
 	
+
+
+	/*
+	png file which allow the apperance of alien 2 on code
+	Location of image: Resources File
+
+	Amount of point that will be added to the score bored 
+	after one of this aliens is destroyed
+	*/
 	@Override
 	public String getImageName() {
-		return "resources/invader1.png";
+		return "resources/alien77.png";
 	}
 	
 	@Override
 	public int getPoints() {
-		return 10;
+		return 30;
 	}
 	
 }
