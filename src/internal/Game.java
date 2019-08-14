26273666970f2b2dcfa9f4d73d1f270c428db06d
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2019-08-13 21:23:19 -0600

    Update Game.java

--- src/internal/Game.java
+++ src/internal/Game.java
@@ -6,41 +6,49 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class Game {
+	//Setting instance variables.
     public static Game main;
     private Window window;
     private Input input;
     private Screen screen;
-
+    
+    //Constructor
     public Game(Window window) {
         this.window = window;
         input = new Input();
         input.registerWindowInputs(window);
         main = this;
     }
-
+    
+    //Getter for input
     public Input getInput() {
         return input;
     }
-
+    
+    //Setter for screen
     public void setScreen(Screen newScreen) {
         if (screen != null) screen.dispose();
         screen = newScreen;
         screen.create();
     }
-
+    
+    //Getter for width
     public float getWidth() {
         return window.getWidth();
     }
-
+    
+    //Getter for height
     public float getHeight() {
         return window.getHeight();
     }
-
+    
+    //This method generates the animation and starts the game loop
     void startGameLoop() {
         new AnimationTimer() {
             private long prev = System.nanoTime();
 
             @Override
+            //This implements the Java FX Animation Timer handler
             public void handle(long now) {
                 float diff = (now - prev) / 1000000000.0f;
                 diff = Math.min(diff, 1.0f/30.0f);
