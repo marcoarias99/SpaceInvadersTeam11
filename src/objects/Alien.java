abdec332846e10bf5e188b4dd29e52f088fd44d0
mohamedmohamed1 <52938358+mohamedmohamed1@users.noreply.github.com>
2019-08-13 22:06:26 -0600

    Update Alien.java

--- src/objects/Alien.java
+++ src/objects/Alien.java
@@ -1,49 +1,46 @@
 package objects;
 
-import internal.Game;
-
 public class Alien extends GameObject {
+	
+	// Instance variables for the width, height, movement speed, direction of Alien 1
+	
     public static final float WIDTH = 22.0f;
     public static final float HEIGHT = 36.0f;
     private float movement_speed = 40.0f;
     private Direction direction = Direction.RIGHT;
-    private Bomb bomb;
+    
+    // Constructor
 
     public Alien(float x, float y) {
         initAlien(x, y);
     }
+    
+    //  Updates the position of Alien 1
 
     @Override
     public void update(float deltaTime) {
         position.x += movement_speed * deltaTime * direction.value;
-        updateBomb(deltaTime, Game.main);
     }
-
-    private void updateBomb(float deltaTime, Game game) {
-        if (bomb != null) {
-            bomb.update(deltaTime);
-            if (bomb.getPosition().y > game.getHeight()) {
-                bomb = null;
-            }
-        }
-    }
-
+    
     private void initAlien(float x, float y) {
         getPosition().set(x, y);
     }
+    
+    // Generates bombs for Alien 1
 
-    public void spawnBomb() {
-        bomb = new Bomb(getPosition().x, getPosition().y);
+    public Bomb getNewBomb() {
+        Bomb bomb = new Bomb(getPosition().x, getPosition().y);
         bomb.getSize().set(Bomb.WIDTH, Bomb.HEIGHT);
-    }
-
-    public Bomb getBomb() {
         return bomb;
     }
+    
+    // Getter for the instance variable direction
 
     public Direction getDirection() {
         return direction;
     }
+    
+    // Setter for the instance variable direction
 
     public void setDirection(Direction direction) {
         this.direction = direction;
@@ -59,14 +56,25 @@ public class Alien extends GameObject {
         }
     }
     
+    // Adds the movement speed of Alien 1
+    
     public void addMovementSpeed(float f) {
     	movement_speed += f;
     }
+    
+    // Setter method for the movement speed of Alien 1
+    
+    public void setMovementSpeed(float f) {
+    	movement_speed = f;
+    }
 
+    // Getter for the image, so that it can be imported for Alien 1, and displays it
 	@Override
 	public String getImageName() {
-		return "resources/alien.png";
+		return "resources/invader2.png";
 	}
+	
+	// Getter for the points, indicates every time alien 1 is killed it returns 5 points 
 	
 	public int getPoints() {
 		return 5;
