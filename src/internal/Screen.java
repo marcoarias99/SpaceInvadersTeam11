7ceb7bd15a66184998c33dd3e796e4c84e353b32
chadigarzeddine1 <52938409+chadigarzeddine1@users.noreply.github.com>
2019-08-13 21:25:53 -0600

    Update Screen.java

--- src/internal/Screen.java
+++ src/internal/Screen.java
@@ -3,6 +3,7 @@ package internal;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface Screen {
+	//Deals with the creation of the main screen
     void create();
     void update(float deltaTime);
     void render(GraphicsContext g);
