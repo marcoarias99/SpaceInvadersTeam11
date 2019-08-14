3edf41859ab13694afa73f90e9ba1367a24c944c
mohamedmohamed1 <52938358+mohamedmohamed1@users.noreply.github.com>
2019-08-13 22:08:04 -0600

    Update Bomb.java
    added comments

--- src/objects/Bomb.java
+++ src/objects/Bomb.java
@@ -1,18 +1,27 @@
 package objects;
 
 public class Bomb extends GameObject {
+	
+	// Instance Variables for width, height, drop_speed and bullet speed
+	
     public static final float WIDTH = 10.0f;
     public static final float HEIGHT = 10.0f;
     public static final float DROP_SPEED = 200.0f;
     private float bulletSpeed = DROP_SPEED;
+    
+    // Constructor
 
     public Bomb(float x, float y) {
         initBomb(x, y);
     }
     
+    // Setter method for the speed, sets the bulletSpeed equal to the speed
+    
     public void setSpeed(float speed) {
     	bulletSpeed = speed;
     }
+    
+    // Updates the position
 
     @Override
     public void update(float deltaTime) {
@@ -23,9 +32,11 @@ public class Bomb extends GameObject {
         getPosition().x = x;
         getPosition().y = y;
     }
+    
+    // Getter for the image for the bomb, so it can display the gif of the bomb
 
 	@Override
 	public String getImageName() {
-		return "resources/bomb.png";
+		return "resources/shot.gif";
 	}
 }
